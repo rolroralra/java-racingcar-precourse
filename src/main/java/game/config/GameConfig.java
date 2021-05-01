069f5b3dc514e1e7fe4d78b707218ed2a957dbfe
rolroralra <rolroralra@gmail.com>
2021-05-01 21:49:48 +0900

    test: GameConfig Implementation

---
+++ src/main/java/game/config/GameConfig.java
@@ -0,0 +1,90 @@
+package game.config;
+
+import game.constants.MessageKey;
+import lombok.Getter;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+@Getter
+public class GameConfig {
+    public static final String MESSAGE_BUNDLE_NAME = "message";
+    private static final InputStream DEFAULT_INPUT_STREAM = System.in;
+    private static final OutputStream DEFAULT_OUTPUT_STREAM = System.out;
+    private static final Locale DEFAULT_LOCALE = Locale.getDefault();
+
+    private final InputStream inputStream;
+    private final OutputStream outputStream;
+    private final Locale locale;
+    private final ResourceBundle resourceBundle;
+
+    public static class Builder {
+        private InputStream inputStream;
+        private OutputStream outputStream;
+        private Locale locale;
+
+        public Builder() {
+            this.inputStream = DEFAULT_INPUT_STREAM;
+            this.outputStream = DEFAULT_OUTPUT_STREAM;
+            this.locale = DEFAULT_LOCALE;
+        }
+
+        public Builder inputStream(InputStream inputStream) {
+            this.inputStream = inputStream;
+            return this;
+        }
+
+        public Builder outputStream(OutputStream outputStream) {
+            this.outputStream = outputStream;
+            return this;
+        }
+
+        public Builder locale(Locale locale) {
+            this.locale = locale;
+            return this;
+        }
+
+        public Builder locale(String languageTag) {
+            this.locale = Locale.forLanguageTag(languageTag);
+            return this;
+        }
+
+        public GameConfig build() {
+            return new GameConfig(this.inputStream, this.outputStream, this.locale);
+        }
+    }
+
+    private GameConfig(InputStream inputStream, OutputStream outputStream, Locale locale) {
+        this.inputStream = inputStream;
+        this.outputStream = outputStream;
+        this.locale = locale;
+        this.resourceBundle = ResourceBundle.getBundle(MESSAGE_BUNDLE_NAME, locale);
+    }
+
+    public static GameConfig getDefault() {
+        return new GameConfig(DEFAULT_INPUT_STREAM, DEFAULT_OUTPUT_STREAM, DEFAULT_LOCALE);
+    }
+
+    public String getMessage(String key) {
+        return this.resourceBundle.getString(key);
+    }
+
+    public String getMessage(MessageKey key) {
+        return this.getMessage(key.name());
+    }
+
+//    private String convertEncoding(String input) {
+//        return this.convertEncoding(input, StandardCharsets.ISO_8859_1, StandardCharsets.UTF_8);
+//    }
+//
+//    private String convertEncoding(String input, Charset from, Charset to) {
+//        return new String(input.getBytes(from), to);
+//    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+}
---
+++ src/test/java/game/config/GameConfigTest.java
@@ -0,0 +1,68 @@
+package game.config;
+
+import game.constants.MessageKey;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameConfigTest {
+
+    @DisplayName("기본값_테스트")
+    @Test
+    public void 기본값_테스트() {
+        GameConfig config = GameConfig.getDefault();
+        assertThat(config)
+                .isNotNull()
+                .hasNoNullFieldsOrProperties()
+                .returns(System.in, GameConfig::getInputStream)
+                .returns(System.out, GameConfig::getOutputStream)
+                .returns(Locale.getDefault(), GameConfig::getLocale)
+                .returns(ResourceBundle.getBundle(GameConfig.MESSAGE_BUNDLE_NAME), GameConfig::getResourceBundle);
+    }
+
+    @DisplayName("빌더_통한_객체_생성_테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"en", "ko"})
+    public void 빌더_통한_객체_생성_테스트(String languageTag) {
+        GameConfig config = GameConfig.builder()
+                .inputStream(System.in)
+                .outputStream(System.out)
+                .locale(languageTag)
+                .build();
+
+        assertThat(config)
+                .isNotNull()
+                .hasNoNullFieldsOrProperties();
+    }
+
+    @DisplayName("메시지_리소스_번들이_가진_메시지_값_비교")
+    @ParameterizedTest
+    @ValueSource(strings = {"en", "ko"})
+    public void 메시지_리소스_번들이_가진_메시지_값_비교(String languageTag) {
+        GameConfig config = GameConfig.builder()
+                .inputStream(System.in)
+                .outputStream(System.out)
+                .locale(languageTag)
+                .build();
+        Map<String, String> messageMap = Arrays.stream(MessageKey.values())
+                .collect(Collectors.toMap(Enum::name, config::getMessage));
+
+        assertThat(convertResourceBundleToMap(config.getResourceBundle())).containsExactlyInAnyOrderEntriesOf(messageMap);
+    }
+
+    private Map<String, String> convertResourceBundleToMap(ResourceBundle resource) {
+        Map<String, String> map = new HashMap<>();
+        Enumeration<String> keys = resource.getKeys();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            map.put(key, resource.getString(key));
+        }
+        return map;
+    }
+}
